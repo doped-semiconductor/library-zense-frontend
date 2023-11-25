7f8f6482c5ff8735fba084ffc73a2ed5d0b7d42a
Ananthakrishna-K-13 <Ananthakrishna.K@iiitb.ac.in>
2023-11-25 16:14:28 +0530

    Completed login and forgot password page

--- library-portal-frontend/app/login/forgot/page.tsx
+++ library-portal-frontend/app/login/forgot/page.tsx
@@ -1,23 +1,32 @@
 import Image from 'next/image'
 import './forgot.css'
+import Link from 'next/link'
 
 const page = () => {
   return (
-    <div className={`login-screen w-screen h-screen flex justify-center items-center`}>
-        <div className='login bg-white rounded-2xl flex-col flex items-center p-4 gap-10'>
-            <Image src='/logo.png' alt='IIITB logo' width={300} height={282}/>
-            <div className='login_inputs flex-col flex items-center '>
-                <div className='helper-message border-solid border-black border rounded-xl flex mb-10'>
-                    <div className='blue-marker w-16 h-full rounded-l-xl'></div>
-                    <p className='p-3'>Please enter your username or email address. You will receive an email message with instructions on how to reset your password.</p>
-                </div>
-                <h1 className=' p-1 text-sm font-extrabold w-full'>ENTER YOUR USERNAME/ E-MAIL ADDRESS</h1>
-                <input type="text" className='border-solid border-black border rounded-2xl w-full h-16 text-xl p-3 text-black mb-7' />
-                <div className='w-full flex justify-end px-1'>
-                <button className='btn px-5 h-11 rounded-3xl font-bold text-white' >GET NEW PASSWORD</button>
-                </div>
+    <div className={`login-screen w-screen min-h-screen flex justify-center items-center`}>
+      <div className='welcometext text-black flex flex-col justify-center items-center '>
+        <h1 className='p-2 text-9xl font-semibold '>Welcome</h1>
+        <h2 className='p-2 text-5xl font-semibold'>to IIITB's Library Portal</h2>
+        <Link href='/register' className='mt-10 font-bold text-white btn rounded-xl p-4'>Not a Member? SignUp</Link>
+      </div>
+      <div className='login rounded-2xl flex-col flex items-center p-4 gap-16 border-solid border-black border max-w-xl max-h-screen text-black'>
+          <Image src='/logo.png' alt='IIITB logo' width={300} height={282}/>
+          <div className='login_inputs flex-col flex items-center '>
+            <div className='border-solid border-black border rounded-l p-3 mb-6'>
+            Please enter your username or email address. You will receive an email message with instructions on how to reset your password.
             </div>
-        </div>
+              <h1 className='text-xl font-semibold w-full'>Username</h1>
+              <input type="text" className='w-full h-14 text-l text-black mb-7' />
+              <div className='flex justify-between w-full p-2'>
+                <div className='flex items-center justify-center '>
+                  <Link href='/login' className='text-black font-medium pl-2'>
+                  <i className="fa-solid fa-arrow-left"></i>&nbsp;Back to Login</Link>
+                </div>                 
+                <button className='btn p-3 rounded-xl font-bold text-white' >Get New Password</button> 
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
--- library-portal-frontend/app/page.tsx
+++ library-portal-frontend/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Navbar from './components/navbar/navbar'
 
 export default function Home() {
   return (
+    <>
+    <Navbar></Navbar>
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
       <p className='font-bold text-xl'> Home Page </p>
     </main>
+    </>
   )
 }
