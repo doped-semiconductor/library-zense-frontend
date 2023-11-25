126c31345190171f82c238d27c050e52bb59d518
Jashwanth Kadaru <jashwanthkadaru2021@gmail.com>
2023-11-25 14:39:10 +0530

    Pushing all root changes & dependency changes in package.json

--- library-portal-frontend/app/not-found.tsx
+++ library-portal-frontend/app/not-found.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notfound = () => {
   return (
-    <div className=' bg-grey text-black w-screen h-screen flex items-center justify-center text-xl font-medium'><span className='text-3xl'>404 |</span>&nbsp;This page could not be found</div>
+    <div className=' bg-gray-300 text-black w-screen h-screen flex items-center justify-center text-xl font-medium'><span className='text-3xl'>404 |</span>&nbsp;This page could not be found</div>
   )
 }
 
